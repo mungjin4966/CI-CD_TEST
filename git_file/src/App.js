0b0afb06d36fca42fc11aaa6366d4e08ebaae9ee
pjhhs021 <pjhhs021@gmail.com>
2022-09-15 14:14:54 +0900

    test:브랜치 테스트중

--- git_file/src/App.js
+++ git_file/src/App.js
@@ -15,16 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-<<<<<<< HEAD
-          Learn React rkskkrksjlkajgoiwjgljlk
-=======
-          <div>해가 중천이라고!!!!!!!!</div>
->>>>>>> c052c2fe1122f30c7ddcbd2d612198305c1ef403
+          Learn React rkskkrksjlkajgoiwjgljlkgitaaa
         </a>
       </header>
-
-
-      <div>왜!! 왜!! 나에 대한 기준만!! 엄격한건데!!!!!</div>
     </div>
   );
 }
